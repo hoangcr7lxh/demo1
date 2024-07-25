f10e21ec3def904489ad8773a4136666446a17fd
unknown <hoangcr7777@gmail.com>
2024-07-25 18:20:24 +0700

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,11 @@ module.exports = {
           h1: ["72px", "98px"],
           h2: ["48px", "64px"],
           h3: ["24px", "32px"],
+          h4: ["40px", "54px"],
           lead1: ["18px", "32px"],
           subtitle2: ["18px", "28px"],
+          Subtitlle3: ["16px", "26px"],
+          Subtitlle4: ["14px", "24px"],
         },
         fontFamily: {
           inter: "Inter",
